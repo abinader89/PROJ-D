8f1c9f98fb56070179ae2dc82f25be1e3bbd9a5c
Abinader <abinader@ccs.neu.edu>
2016-11-25 12:53:00 -0500

    Starting off. Created the main.

--- src/CS3200FinalProject.java
+++ src/CS3200FinalProject.java
@@ -2,4 +2,8 @@
  * Created by Abinader on 11/22/16.
  */
 public class CS3200FinalProject {
+  
+  public void run() {
+    System.out.println("NOTHING YET.");
+  }
 }
---
+++ src/MainRunner.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Abinader on 11/24/16.
+ */
+public class MainRunner {
+  public static void main(String[] args) {
+    CS3200FinalProject app = new CS3200FinalProject();
+    app.run();
+  }
+}
